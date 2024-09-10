43227889a4220e4c89c7031181fce7db1d00ed83
LawSoul <lawrencechan@Lawrences-MacBook-Air.local>
2024-09-10 15:43:16 -0700

    even more

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 import math
 import time
-name =  "law"
-#print(len(name))
-#print(name.find("l"))
+#name =  "law"
+#print(len())
+#print(name.find())
 #print(name.capitalize())
 #print(name.upper())
 #print(name.lower())
 #print(name.isdigit())
 #print(name.isalpha())
 #print(name.count)
-#print(name.replace("a","o"))
+#print(name.replace("",""))
+
 #y = str(y)
 #y = int(y)
 #y = float(y)
@@ -41,13 +42,46 @@ name =  "law"
 #food.sort()
 #food.clear()
 
+#drink = ["water", "soda"]
+#breakfast = ["pizza","Salad"]
+#meal = [drink, breakfast]
+#print(meal[2][2])
 
+#helpr = ("bro", 21, "ja")
+#list.count
+#list.index
+#if "" in drink:
 
+#set = {"spoon", "fork", "knife"}
+#dish = {"bowl","plate"}
+#set.add()
+#set.remove()
+#set.clear()
+#set.update(dish)
+#dine = set.union(dish)
+#print(set.difference(dish))
+#print(set.intersection(dish))
 
+#capitals = {'USA': 'D.C', 'Japan':'Tokyo', 'China': 'Beijing'}
+#capitals.update({'South Korea': 'Seoul'})
+#capitals.update({'USA': 'SF'})
+#capitals.pop()
+#capitals.clear()
+#print(capitals['Japan'])
+#print(capitals.get('South Korea'))
+#print(capitals.keys())
+#print(capitals.values())
+#print(capitals.items())
+#for key,value in capitals.items():
+#    print(key, value)
 
-
-
-
+#name = "George Ian"
+#if name[0].islower():
+#    name = name.capitalize
+#last_name = name[7:].lower()
+#first_name = name[:6].upper()
+#last_character = name[-1]
+#print(last_name)
 
 
 
