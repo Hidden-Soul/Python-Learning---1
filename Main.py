fe1b8154e3d935162fe73dd1d33ffc3da7bff2e3
LawSoul <lawrencechan@Lawrences-MacBook-Air.local>
2024-09-10 15:42:30 -0700

    More functions

--- Main.py
+++ Main.py
@@ -1,1 +1,61 @@
-print("hello world")
+import math
+import time
+name =  "law"
+#print(len(name))
+#print(name.find("l"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count)
+#print(name.replace("a","o"))
+#y = str(y)
+#y = int(y)
+#y = float(y)
+#text = input("Who are you?:")
+#round(y)
+#math.ceil(x)
+#math.floor(x)
+#abs(x)
+#pow(x,2)
+#math.sqrt(420)
+#min(x,y)
+#max(x,y)
+
+#name = name[:2]
+#print(name)
+#name = name[2:]
+#name = name[::2]
+#name = name[::-1]
+#print(name)
+#slice = slice(7,-4)
+#print(name[slice])
+#time.sleep
+
+#food = ["sushi", "icecream", "pizza"]
+#food.append("Water")
+#food.remove("Water")
+#food.pop()
+#food.insert(0, "cake")
+#food.sort()
+#food.clear()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
