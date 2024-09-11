161c92202289668663d2e22ac538ac269bcbbacd
LawSoul <lawrencechan@Lawrences-MacBook-Air.local>
2024-09-10 22:55:39 -0700

    code new

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import math
 import time
+import random
 #name =  "law"
 #print(len())
 #print(name.find())
@@ -82,6 +83,114 @@ import time
 #first_name = name[:6].upper()
 #last_character = name[-1]
 #print(last_name)
+
+#def hello(Thing,age,last_name):
+#    print("hello" + Thing)
+#hello(" bro")
+#def multiply(num1, num2):
+#    return num1 * num2
+#print(multiply(6,8))   
+#hello(Thing="bro",age=21,last_name="code")
+
+#print(round(abs(float(input("Enter a whole postive num:")))))
+
+#Global/Local variable
+
+#def add(*args):
+#    sum = 0
+#    args = list(args)
+#    args[0] = 0
+#    for i in args:
+#        sum += i
+#    return sum
+#print(add(1,2,3,4))
+
+#def hello(**kwargs):
+#    print("My " + kwargs['first'] + " and " + kwargs['last'])
+#    print("Hello",end=" ")
+#    for key, value in kwargs.items():
+#       print(value,end=" ")
+#hello(first="Law",last="Sol")
+
+#animal = "cow"
+#item = "moon"
+#print("The {} jumped over the {}".format(animal,item))
+##print("The {1} jumped over the {0}".format(animal,item))
+#print("The {animal} jumped over the {item}".format(animal="cow",item="moon"))
+#Text = "The {} jumped over {}"
+#print(text.format(animal,item))
+#print("My name is {:<10}".format(name))
+#print("My name is {:>10}".format(name))
+#print("My name is {:^10}".format(name))
+#num = 3.14
+#print(" The num pi is {:.3f}".format(num))
+#print(" The num pi is {:b}".format(num))
+#print(" The num pi is {:o}".format(num))
+#print(" The num pi is {:,}".format(num))
+#print(" The num pi is {:X}".format(num))
+#print(" The num pi is {:E}".format(num))
+
+#x = random.randint(1,5)
+#y = random.randint()
+#myList = ['rock','paper','scissors']
+#z = random.choice(myList)
+#cards = [1,2,3,4,5,6,7,8,9,"J"]
+#random.shuffle(cards)
+#print(z)
+
+#try:    
+#    numerator = int(input("Enter a num to divide: "))
+#    denominator = int(input("Enter a num to divide by: "))
+#    result = numerator / denominator
+#except ZeroDivisionError as e:
+#    print(e)
+#    print("You can't divide by zero")
+#except ValueError as e:
+#    print(e)
+#    print("Enter only numbers")
+#except Exception as e:
+#    print(e)
+#    print("Some is wrong")
+#else:
+#    print(result)
+#finally:
+#    print("This will always execute")
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
